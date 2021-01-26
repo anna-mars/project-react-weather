6ee3b23997f2ef8f1387bf4894e20309cd1aa971
anna-mars <73495328+anna-mars@users.noreply.github.com>
2021-01-26 21:44:55 +0100

    Added open-source-code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App</h1>
+      <footer>
+        This project was coded by Anna Frac and is {""}
+        <a
+          href="https://github.com/anna-mars/project-react-weather"
+          target="-blank"
+        >
+          open-sourced an GITHUB
+        </a>
+      </footer>
     </div>
   );
 }
