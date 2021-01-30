f9931dc0eac912c02d23bcddd3fb6dc3856112e4
anna-mars <73495328+anna-mars@users.noreply.github.com>
2021-01-30 16:38:02 +0100

    Added bootstrap to the Weather app.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Anna Frac and is {""}
-        <a
-          href="https://github.com/anna-mars/project-react-weather"
-          target="-blank"
-        >
-          open-sourced an GITHUB
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Anna Frac and is {""}
+          <a
+            href="https://github.com/anna-mars/project-react-weather"
+            target="-blank"
+          >
+            open-sourced an GITHUB
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
