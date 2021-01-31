7a97447a68f6c754ade658959d3a6f1cf44a5797
anna-mars <73495328+anna-mars@users.noreply.github.com>
2021-01-31 20:25:39 +0100

    Added API/axios integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Stalowa Wola" />
         <footer>
           This project was coded by Anna Frac and is {""}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,79 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="row">
-        <div className="col-6">
-          <form>
-            <input
-              type="search"
-              placeholder="Search for a city"
-              className="form-control"
-              autoFocus="on"
-            />
-          </form>
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ done: false });
+  function handleResponse(response) {
+    setWeatherData({
+      city: response.data.name,
+      country: response.data.sys.country,
+      date: "Sun 7:00",
+      time: "28.12.2020",
+      temperature: Math.round(response.data.main.temp),
+      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      temperatureMax: Math.round(response.data.main.temp_max),
+      temperatureMin: Math.round(response.data.main.temp_min),
+      temperatureFeels: Math.round(response.data.main.feels_like),
+      sky: response.data.weather[0].main,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      pressure: response.data.main.pressure,
+      done: true,
+    });
+  }
+  if (weatherData.done) {
+    return (
+      <div className="Weather">
+        <div className="row">
+          <div className="col-6">
+            <form>
+              <input
+                type="search"
+                placeholder="Search for a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </form>
+          </div>
+          <div className="col-6">
+            <input type="submit" value="🔍" className="btn " />
+            <button className="btn ">📍</button>
+          </div>
         </div>
-        <div className="col-6">
-          <input type="submit" value="🔍" className="btn " />
-          <button className="btn ">📍</button>
+        <div className="basic-weather">
+          <h1>
+            <span>{weatherData.city}</span>,<span>{weatherData.country}</span>
+          </h1>
+          <ul>
+            <li>
+              <span>{weatherData.date}</span>
+            </li>
+            <li>{weatherData.time}</li>
+          </ul>
         </div>
-      </div>
-      <div className="basic-weather">
-        <h1>
-          <span>Amsterdam</span>,<span>NL</span>
-        </h1>
-        <ul>
-          <li>
-            <span>Sun 7:00</span>
-          </li>
-          <li>28.12.2020</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="clearfix ">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Rain"
-              class="float-left"
-            />
-            <div className=" float-left">
-              <span className="temperature">28</span>
-              <span className="units">°C |°F</span>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="clearfix ">
+              <img src={weatherData.icon} alt="Rain" class="float-left" />
+              <div className=" float-left">
+                <span className="temperature">{weatherData.temperature}</span>
+                <span className="units">°C |°F</span>
+              </div>
+            </div>
+            <div className="present-temp">
+              <ul>
+                <li className="main-max">
+                  Max : {weatherData.temperatureMax} °
+                </li>
+                <li className="main-min">
+                  Min : {weatherData.temperatureMin} °
+                </li>
+                <li className="feels-like">
+                  Feels like : {weatherData.temperatureFeels}°
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="present-temp">
+          <div className="col-md-6">
             <ul>
-              <li className="main-max">
-                Max : <span>28</span>°
-              </li>
-              <li className="main-min">
-                Min : <span>23</span>°
-              </li>
-              <li className="feels-like">
-                Feels like : <span>23</span>°
-              </li>
+              <li>Sky: {weatherData.sky}</li>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+              <li>Pressure: {weatherData.pressure} hPa</li>
             </ul>
           </div>
         </div>
-        <div className="col-md-6">
-          <ul>
-            <li>
-              Sky: <span>Rain</span>
-            </li>
-            <li>
-              Humidity: <span>93</span> %
-            </li>
-            <li>
-              Wind: <span>43</span> km/h
-            </li>
-            <li>
-              Pressure: <span>1007</span> hPa
-            </li>
-          </ul>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "58a0b6e26263101abdfda8d9e9d3a0f0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return " Loading...";
+  }
 }
